86d2be031b91a447a41afd82d7c1c8c9fcac975c
chen6665758 <332965198@qq.com>
2019-11-05 14:17:59 +0800

    列表接口完成

--- app/src/main/java/com/cg/citymanage/EventWaitActivity.java
+++ app/src/main/java/com/cg/citymanage/EventWaitActivity.java
@@ -3,14 +3,24 @@ package com.cg.citymanage;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.cg.citymanage.Adapters.EventOverviewListAdpater;
+import com.cg.citymanage.infos.Constants;
 import com.cg.citymanage.models.EventListModel;
+import com.cg.citymanage.untils.myUntils;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
 import com.wuxiaolong.pullloadmorerecyclerview.PullLoadMoreRecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +54,8 @@ import java.util.List;
 */
 public class EventWaitActivity extends BaseActivity implements View.OnClickListener {
 
+    private String appToken;
+
     /**
      * 标题栏
      */
@@ -68,7 +80,7 @@ public class EventWaitActivity extends BaseActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
 
         mContext = this;
-
+        appToken = mSharedPreferences.getString("appToken","");
         initControls();
     }
 
@@ -101,11 +113,14 @@ public class EventWaitActivity extends BaseActivity implements View.OnClickListe
                 //下拉刷新
                 pageNo = 1;
                 list_data.clear();
+                initData();
             }
 
             @Override
             public void onLoadMore() {
                 //上拉加载更多
+                pageNo++;
+                initData();
             }
         });
 
@@ -121,7 +136,81 @@ public class EventWaitActivity extends BaseActivity implements View.OnClickListe
         //无数据时显示
         rela_NoData = (RelativeLayout)findViewById(R.id.rela_NoData);
 
-        tempData();
+        //tempData();
+        initData();
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData()
+    {
+        progress_Dialog.show();
+        OkGo.<String>post(Constants.EVENTEVENTWAIT_URL)
+                .tag(this)//
+                .params("access_token", appToken)
+                .params("page",String.valueOf(pageNo))
+                .params("pageSize",Constants.PAGESIZE)
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+                        //注意这里已经是在主线程了
+                        progress_Dialog.dismiss();
+                        String data = response.body();
+                        try {
+                            JSONObject json = new JSONObject(data);
+                            String resultCode = json.getString("code");
+                            if(resultCode.equals("2000"))
+                            {
+                                JSONArray array = json.getJSONArray("data");
+                                if(array.length() > 0)
+                                {
+                                    for(int i = 0;i<array.length();i++) {
+                                        JSONObject child = array.getJSONObject(i);
+                                        EventListModel model = new EventListModel();
+                                        model.setEventId(child.getString("id"));
+                                        model.setEventName(child.getString("eventTitle"));
+                                        model.setEventTime(TextUtils.isEmpty(child.getString("createTime")) ? "" :
+                                                myUntils.StringPattern(child.getString("createTime"),"yyyy-MM-dd HH:mm:ss","yyyy-MM-dd"));
+                                        model.setEventInfo(child.getString("eventCode") + " " + child.getString("eventTypeName"));
+                                        model.setEventLink(child.getString("eventStatus"));
+                                        model.setImpatient(false);
+                                        list_data.add(model);
+                                    }
+                                    eAdapter.notifyDataSetChanged();
+
+                                }else{
+                                    myUntils.showToast(mContext,"没有新的事件了！");
+                                }
+
+                            }else{
+                                myUntils.showToast(mContext,json.getString("message"));
+                            }
+
+                            //根据数据数量，来判断是否给出无数据提示
+                            if(list_data.size() > 0)
+                            {
+                                rela_NoData.setVisibility(View.GONE);
+                            }else{
+                                rela_NoData.setVisibility(View.VISIBLE);
+                            }
+                        }catch (Exception ex)
+                        {
+                            Log.e("EventWait", "行数: 199  ex:" + ex.getMessage());
+                            myUntils.showToast(mContext,"请检查网络是否正常链接！");
+                            return;
+                        }
+
+                        pmr_event.setPullLoadMoreCompleted();
+                    }
+
+                    @Override
+                    public void onError(Response<String> response) {
+                        super.onError(response);
+                        progress_Dialog.dismiss();
+                        Log.e("EventWait", "行数: 211  error:" + response.body());
+                    }
+                });
     }
 
     @Override
@@ -148,19 +237,4 @@ public class EventWaitActivity extends BaseActivity implements View.OnClickListe
         }
     }
 
-    public void tempData()
-    {
-        for(int i=0;i<10;i++)
-        {
-            EventListModel model = new EventListModel();
-            model.setEventId(String.valueOf(i+1));
-            model.setEventName("待办第" + i + "事件");
-            model.setEventTime("2019-10-" + (i+1));
-            model.setEventLink("结果反馈" + i);
-            model.setEventInfo("203032019102100"+i+":施工废弃料");
-            list_data.add(model);
-        }
-
-        eAdapter.notifyDataSetChanged();
-    }
 }
