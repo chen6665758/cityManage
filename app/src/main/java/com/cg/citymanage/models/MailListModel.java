f735b06b1af21934f56679dc8cbf68938e4da5c5
chen6665758 <332965198@qq.com>
2019-11-18 14:52:41 +0800

    详情接口完成

---
+++ app/src/main/java/com/cg/citymanage/models/MailListModel.java
@@ -0,0 +1,80 @@
+package com.cg.citymanage.models;
+
+import com.nanchen.wavesidebar.FirstLetterUtil;
+
+/**
+/*                       _oo0oo_
+/*                      o8888888o
+/*                      88" . "88
+/*                      (| -_- |)
+/*                      0\  =  /0
+/*                    ___/`---'\___
+/*                  .' \\|     |// '.
+/*                 / \\|||  :  |||// \
+/*                / _||||| -:- |||||- \
+/*               |   | \\\  -  /// |   |
+/*               | \_|  ''\---/''  |_/ |
+/*               \  .-\__  '-'  ___/-. /
+/*             ___'. .'  /--.--\  `. .'___
+/*          ."" '<  `.___\_<|>_/___.' >' "".
+/*         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+/*         \  \ `_.   \_ __\ /__ _/   .-` /  /
+/*     =====`-.____`.___ \_____/___.-`___.-'=====
+/*                       `=---='
+/*
+/*     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*               佛祖保佑         永无BUG
+
+* 功能：通讯录列表model
+* 作者：cg
+* 时间：2019/11/18 11:09
+*/
+public class MailListModel {
+
+    private String index;
+    private String id;
+    private String tel;
+    private String name;
+
+    public MailListModel() {
+    }
+
+    public MailListModel(String id,String tel, String name) {
+        this.index = FirstLetterUtil.getFirstLetter(name);
+        this.id = id;
+        this.tel = tel;
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
