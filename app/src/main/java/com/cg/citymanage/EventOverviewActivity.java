bf5c3084c0e1c0a6081ca2efb12b0039c1bc66eb
chen6665758 <332965198@qq.com>
2019-12-06 08:31:19 +0800

    详情接口完成

--- app/src/main/java/com/cg/citymanage/EventOverviewActivity.java
+++ app/src/main/java/com/cg/citymanage/EventOverviewActivity.java
@@ -1,16 +1,12 @@
 package com.cg.citymanage;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bigkoo.pickerview.builder.TimePickerBuilder;
 import com.bigkoo.pickerview.listener.OnTimeSelectListener;
